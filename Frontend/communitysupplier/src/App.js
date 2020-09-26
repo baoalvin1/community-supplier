a27450bf2ef56f398de063d20413c3c3e356ab7d
ArmaniL <35434507+ArmaniL@users.noreply.github.com>
2020-09-26 16:09:51 -0400

    Finalized Card design

--- Frontend/communitysupplier/src/App.js
+++ Frontend/communitysupplier/src/App.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import './App.css';
 import Heading from "./components/Heading"
+import Card from "./components/Card"
+
+const requests=[
+
+  {
+  hospitalName:"Baptist Hospital",
+  itemType:"Ventilators",
+  quantityAsked:10,
+  address:"Kendall Drive",
+  hasFile:true
+  },
+  
+  {
+    hospitalName:"Miami Hospital",
+    itemType:"Inhalers",
+    quantityAsked: 40 ,
+    address:"109 SW",
+    hasFile:false
+    },
+    {
+      hospitalName:" Fake Hospital",
+      itemType:"Guns",
+      quantityAsked: 2 ,
+      address:"123 SW",
+      hasFile:true
+      },
+  
 
 
+
+]
 
 
 function App() {
@@ -10,7 +39,11 @@ function App() {
     <div className="App">
     <Heading></Heading>
     <div className="grid-container">
-      
+      {requests.map((request)=>{
+        return(
+        <Card {...request} />
+        )
+      })}
     </div>
     </div>
   );
--- Frontend/communitysupplier/src/components/Card.js
+++ Frontend/communitysupplier/src/components/Card.js
@@ -1,10 +1,11 @@
 import React,{Component} from 'react' 
 import genericimage from "../hospital.svg"
-export default class Login extends Component{
+import downloadimage from "../download.svg"
+export default class Card extends Component{
 
 
 constructor(){
-
+super()
 
 }
 
@@ -16,19 +17,34 @@ componentDidMount(){
 }
 //hospital name,number ,type,
 render (){
+const file=this.props.hasFile?<img
+alt=""
+src={downloadimage }
+width="15"
+height="20"
+style={{marginLeft:"90%"}}
+/>:<> </>
+
 return(
   <div className="Card">
-
+      <div style={{borderBottomWidth:"10px"}}>
       <img
         alt=""
         src={this.props.image?this.props.image : genericimage}
-        width="70"
-        height="30"
-      />{' '}
+        width="100"
+        height="40"
+        style={{marginBottom: "2vh",marginTop: "2vh"}}
+      />
+      </div>
+      <div style={{display:"flex",flexDirection:"column"}}>
+      <span style={{fontWeight :"bold"}}>{this.props.hospitalName}</span>
+<span style={{fontStyle:"italic"}}>{this.props.address}</span>
+      <span>{this.props.quantityAsked+" " +this.props.itemType}</span>
+      </div>
+      <button style={{width:"7vh",margin:"auto",marginTop:"2vh",backgroundColor:"rgb(255, 128, 128)",borderColor:"rgb(255, 128, 128)",borderRadius:"0.25rem",whiteSpace:"nowrap",border:"1px solid transparent",padding:".375rem .75rem",lineHeight:"1.5",textAlign:"center"}} >Supply</button>
+      {file}
       
-      <span>{this.props.name}</span>
-      <span>{this.props.amount}</span>
-      <span>{this.props.types}</span>
+      
       
 </div>
 )
