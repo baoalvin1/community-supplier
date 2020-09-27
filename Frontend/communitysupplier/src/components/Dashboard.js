75c52d613d1cce1cb7fe379c4bc70efbd2eb1250
ArmaniL <35434507+ArmaniL@users.noreply.github.com>
2020-09-27 03:53:11 -0400

    Finalized fetch()

--- Frontend/communitysupplier/src/components/Dashboard.js
+++ Frontend/communitysupplier/src/components/Dashboard.js
@@ -2,17 +2,21 @@ import React, { Component } from 'react';
 import Card from "./Card"
 import addimage from "../add.svg"
 
-const requests=[]
+
 
 export default class Dashboard extends Component{
 
+
+constructor(){
+  super()
+  this.state={data:[],fetched:false}
+}
 
 async componentDidMount(){
     const url="https://communitysupplier.uc.r.appspot.com"
     const response = await fetch(url)
     const content =  await response.json();
-    this.setState({data:content.result})
-
+    this.setState({data:content.results,fetched:true})
 
 
 }
@@ -20,17 +24,19 @@ async componentDidMount(){
 
 
 render(){
-return(
+
+  return(
  <>   
   <img style={{marginLeft:"10%",cursor:"pointer"}} src={addimage} width="40vh" height="80vh" onClick={this.props.set}/>
 
 <div className="grid-container">
-  {this.state.data.map((request)=>{
+
+  { this.state.fetched===true ?  this.state.data.map((request)=>{
     return(
     <Card {...request} />
     )
 
-  })}
+  }):<></>}
 </div>
 </>
 );
