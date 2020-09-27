48037aba63d6dd8f199a8f4e67ae202a64a2345e
ArmaniL <35434507+ArmaniL@users.noreply.github.com>
2020-09-26 23:58:49 -0400

    added url

--- Frontend/communitysupplier/src/components/Dashboard.js
+++ Frontend/communitysupplier/src/components/Dashboard.js
@@ -2,67 +2,17 @@ import React, { Component } from 'react';
 import Card from "./Card"
 import addimage from "../add.svg"
 
-const requests=[
-
-    {
-    hospitalName:"Baptist Hospital",
-    itemType:"Ventilators",
-    quantityAsked:10,
-    address:"Kendall Drive",
-    hasFile:true
-    },
-    
-    {
-      hospitalName:"Miami Hospital",
-      itemType:"Inhalers",
-      quantityAsked: 40 ,
-      address:"109 SW",
-      hasFile:false
-      },
-      {
-        hospitalName:" Fake Hospital",
-        itemType:"Guns",
-        quantityAsked: 2 ,
-        address:"123 SW",
-        hasFile:true
-        },
-    {
-        hospitalName:" dakfjv nfv",
-        itemType:"Weapons",
-        quantityAsked: 22 ,
-        address:"123 SW",
-        hasFile:true
-        },
-        {
-        
-        hospitalName:"Among Us",
-        itemType:"Imposter",
-        quantityAsked: -1 ,
-        address:"123 SW",
-        hasFile:true
-        },
-        {
-          hospitalName:"Grand Thheft",
-          itemType:"Weapons Of mass Destruc",
-          quantityAsked: "Infity " ,
-          address:"109 SW",
-          hasFile:false
-          }
-  
-  
-  
-  ]
-  
 
 
 export default class Dashboard extends Component{
 
 
-componentDidMount(){
+async componentDidMount(){
     const url="https://communitysupplier.uc.r.appspot.com"
     const response = await fetch(url)
     const content =  await response.json();
-    
+    this.setState({})
+
 
 
 }
