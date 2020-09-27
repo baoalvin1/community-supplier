2f844b068113dd0349c7bfc886ce50228913a877
Eeister-Git <55165323+Eeister-Git@users.noreply.github.com>
2020-09-26 23:04:38 -0400

    Update backend.py
    template folder got rid of. no longer needed
    root function is no longer needed
    
    importing flask cors enables for reading of REACT app. 
    
    @cors initialize the Flask-Cors extension with default arguments in order to allow CORS for all domains on all routes. https://flask-cors.readthedocs.io/en/latest/

--- backend.py
+++ backend.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from pymongo import MongoClient
 from datetime import date
+from flask_cors import CORS
 
 cluster = MongoClient("mongodb+srv://newuser:Shellhacks2020@cluster0.e2pi8.gcp.mongodb.net/CommunitySupplierDatabase?retryWrites=true&w=majority")
 db = cluster["CommunitySupplierDatabase"]
@@ -20,15 +21,16 @@ d1 = today.strftime("%d/%m/%Y")
     }
 )"""
 
-app = Flask(__name__, template_folder = "Frontend/communitysupplier/public")
+app = Flask(__name__)
+CORS(app) #enables 
 
 
-@app.route('/')
+"""@app.route('/')
 def root():
     # For the sake of example, use static information to inflate the template.
     # This will be replaced with real information in later steps.
 
-    return render_template('index.html')
+    return render_template('index.html')""" # pointless function now as rendering is no longer needed. 
 
 @app.route("/get", methods=["GET"])
 def get():
