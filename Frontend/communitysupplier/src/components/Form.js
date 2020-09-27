765f1706543eb1b9f3c9861faa78d907d99bb0ca
ArmaniL <35434507+ArmaniL@users.noreply.github.com>
2020-09-27 01:31:36 -0400

    Added fetch

--- Frontend/communitysupplier/src/components/Form.js
+++ Frontend/communitysupplier/src/components/Form.js
@@ -8,7 +8,12 @@ export default class Form extends Component{
 constructor(){
 
 super()
-this.state={}
+this.state={
+  hospitalName:"",
+  itemType:"",
+  quantityAsked:"",
+  address:""
+}
 
 }
 
@@ -16,14 +21,14 @@ this.state={}
 
 
     submit(event){
+const url =  "https://communitysupplier.uc.r.appspot.com/"
 event.preventDefault();
-
-/*const response=  await fetch('http://localhost:5000',{method:'POST',headers: {
+const response =  await fetch(url,{method:'POST',headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json'
-  },body: JSON.stringify(data)})
+  },body: JSON.stringify(this.state)})
   const content=await response.json();
-*/
+
     }
 
 
@@ -32,10 +37,10 @@ return(
  < >  <div >
  <form>
    
-   <input type="Name" className="NameEntry"placeholder="Health Provider Name" name="name"/>
-   <input type="address" className="AddressEntry" placeholder="Address"  name="address"/>
-   <input type="number" className="NumberEntry" placeholder="Amount"  name="amount"/>
-   <input type="text" className="AddressEntry" placeholder="Name of Supply"  name="type"/>
+   <input type="Name" className="NameEntry"placeholder="Health Provider Name" onChange={(e)=>{this.setState({hospitalName:e.target.value})}} name="name"/>
+   <input type="address" className="AddressEntry" placeholder="Address" onChange={(e)=>{this.setState({address:e.target.value})}}  name="address"/>
+   <input type="number" className="NumberEntry" placeholder="Amount" onChange={(e)=>{this.setState({quantityAsked:e.target.value})}}   name="amount"/>
+   <input type="text" className="AddressEntry" placeholder="Name of Supply"  onChange={(e)=>{this.setState({itemType:e.target.value})}}   name="type"/>
    
    </form>
    <button className='submit' onClick={(e)=>{this.submit(e)}} >Submit</button>
